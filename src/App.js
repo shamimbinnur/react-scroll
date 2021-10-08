26b1ae5148a1dcd1ca1f074022512ceef3f2d528
shamimbinnur <sbnsbn00@gmail.com>
2021-10-08 22:56:15 +0600

    ye

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import * as Scroll from 'react-scroll';
+import { scrollTo, Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    
+  }, [])
+
+  Events.scrollEvent.register("begin", (to, element)=>{
+    console.log("Begin", to, element);
+  })
+  
+  Events.scrollEvent.register("end", (to, element)=>{
+    console.log("End", to, element);
+  })
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <Link duration={500} smooth to="firstElement">
+          First
+        </Link>
+        <Link duration={500} smooth to="secondElement">
+          Second
+        </Link>
+        <Link duration={500} smooth to="thirdElement">
+          Third
+        </Link>
+        <Link duration={500} smooth to="fourthElement">
+          fourth
+        </Link>
+      </div>
+
+      
+      <Element  name="firstElement" className="element1">
+        First element
+      </Element>
+
+      <Element id="secondElement" className="element2">
+        Second element
+      </Element>
+
+      <Element name="thirdElement"  className="element3">
+        Third element
+      </Element>
+      <Element onClick={()=> scroll.scrollToTop()}  name="fourthElement" className="element4">
+        Third element
+      </Element>
     </div>
   );
 }
